6548353f74080f5c9913c3c34869720a4e9c8507
manchur <manchur.fiverr@gmail.com>
2023-04-02 00:20:01 +0600

    this is login template i make it using django

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from home import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('login', views.login, name="login"),
-    path('logout', views.logout, name="logout"),
+    path('login', views.loginuser, name="login"),
+    path('logout', views.logoutuser, name="logout"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib.auth import logout, login
 
 # password = mnbvcx!@#$
 # Create your views here.
 
 def index(request):
+    if request.user.is_anonymous:
+        return redirect('/login')
+    
     return render(request, "index.html")
 
-def login(request):
+def loginuser(request):
+    if request.method == "POST":
+        username= request.POST.get('username')
+        password= request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request,user)
+            return redirect('/')
+        else:
+            # No backend authenticated the credentials
+            return render(request, "login.html")
+        
     return render(request, "login.html")
 
-def logout(request):
-    return render(request, "logout.html")
+def logoutuser(request):
+    logout(request)
+    return redirect("/login")
